bafac3a4b4fab1337356fca9b2bce53b91abfac6
winter9F <147862480+winter9F@users.noreply.github.com>
2023-12-17 12:12:16 -0800

    Update 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -11,13 +9,23 @@ function App() {
       <div className='body'>
 
 
-        <div className='cont card bg-dark container card mt-5 '>
+        <div className='container cont card bg-dark card mt-5 '>
           <div className="row justify-content-center">
 
             <section className='cont1 bg-white col col-lg-7 p-5'>
-              <h1>Winter Faye</h1>
-              <h6>Software Engineer</h6>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, blanditiis modi. Dolores inventore repudiandae neque facere harum explicabo, ratione, impedit dignissimos dolorem assumenda, quibusdam veritatis doloribus fugit id officia commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic tempore molestiae sed quis blanditiis ipsam illo perferendis similique commodi sunt, aliquam fugiat. Quia sint neque labore voluptatem at! Enim, blanditiis.</p>
+
+              <div className='row mb-2'>
+                <div className='col-9'>
+                  <h1>Winter Faye</h1>
+                  <h6>Software Engineer</h6>
+                </div>
+                <div className='col text-end'>
+                  <a href="https://www.linkedin.com/in/winterfaye/"><img className='logoLinkedIn' src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702833676/Language%20Icons/LinkedIn_icon_vi2xdq.jpg" alt="" /></a>
+                </div>
+              </div>
+              <div>
+                <p>Aspiring Software Engineer with a working knowledge of both backend and frontend development. Currently seeking opportunities, including internships, to apply my skills and learn from experienced professionals. I am passionate about fostering innovation in an inclusive working environment, and eager to make positive contributions to projects while growing within a collaborative team!</p>
+              </div>
               <div>
                 <img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702150439/Language%20Icons/HTML5_logo_i7agog.png" className="logoHtml" alt="" />
 
@@ -27,26 +35,40 @@ function App() {
 
                 <img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702150439/Language%20Icons/JavaScript_logo_rseodv.png" className="logoJs" alt="" />
 
-                <img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702152146/Language%20Icons/Typescript_logo_wxdlan.png" className="logoTs" alt="" />
               </div>
             </section>
 
 
-            <section className=' cont2 d-block col-md ps-5'>
+            <section className=' cont2 d-block col-md'>
+
+
+
+
               <div className='text-end'>
-                <img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1698336656/No_user_image_l3xbpn.jpg" alt="" />
+                <img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702676360/Winter/WinterPic_cvjvgf.jpg" alt="" />
               </div>
+
               <div className='skills pt-3'>
-                <h5>Skills</h5>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi enim repudiandae soluta ratione. Quo incidunt dignissimos tempora minus modi, fugit vero ducimus suscipit quasi dolorum aliquid similique a reiciendis illum?
-                </p>
+
+                <ul>
+                  <h4>Skills</h4>
+                  <li>
+                    Html | Css | JavaScript
+                  </li>
+                  <li>
+                    Express | React | Bootstrap
+                  </li>
+                  <li>
+                    NodeJS | MongoDB | MongoseODM
+                  </li>
+
+                </ul>
               </div>
             </section>
 
 
 
-            <section className='container bg-light border-bottom d-none d-md-flex align-items-center'>
+            <section className='bg-light border-bottom d-none d-md-flex align-items-center'>
               <div className='col text-end p-2'>
                 <a href="https://react.dev/" target="_blank">
                   <img className="logoReact" src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702150439/Language%20Icons/React_Logo_lkaxbz.png" alt="" />
@@ -64,29 +86,32 @@ function App() {
               </div>
             </section>
 
-            <section className='bg-dark'>
-              <section className='card-header'>
-                <h2 className='ProjTitle p-2 mt-3 '>Projects</h2>
-              </section>
+            <section className='bg-dark border-top border-light'>
+              <div className='card-header row d-flex align-items-center'>
+                <div className='col-5 text-start'>
+                  <h2 className='ProjTitle p-2 mt-3'>Projects</h2>
+                </div>
+                <div className='col text-end'>
+                  <a href="https://github.com/winter9F/winter-personal-webpage"><button className='btn btn-danger'>Code for this Page</button></a>
+                </div>
+              </div>
             </section>
 
-            <section className="proj container mb-5 row align-items-center">
+            <section className="proj mb-5 row align-items-center">
               <div className='proj col-12 col-md border border-secondary'>
-                <a href="https://github.com/winter9F/winter-portfolio-1"><button className='btn btn-light'>Code on Github</button></a>
+                <a href="https://github.com/winter9F/winter-social-network-app"><button className='btn btn-light'>Code on Github</button></a>
                 <div className='ratio ratio-16x9 '>
-                  <iframe src="https://www.youtube.com/embed/BrG12PTRiEY?si=KFFMDHFjBQwk1pcf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                  <iframe src="https://www.youtube.com/embed/BrG12PTRiEY?si=KFFMDHFjBQwk1pcf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allow="fullscreen"></iframe>
                 </div>
               </div>
 
               <div className='proj col border border-secondary '>
-                <a href="https://github.com/winter9F/winter-portfolio-2"><button className='btn btn-light'>Code on Github</button></a>
+                <a href="https://github.com/winter9F/winter-notes-app"><button className='btn btn-light'>Code on Github</button></a>
                 <div className='ratio ratio-16x9 '>
-                  <iframe src="https://www.youtube.com/embed/DRFvH5xaoLw?si=XFq-MAoWcHPxew5Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                  <iframe src="https://www.youtube.com/embed/DRFvH5xaoLw?si=XFq-MAoWcHPxew5Z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allow="fullscreen" ></iframe>
                 </div>
               </div>
             </section>
-
-            <a href=""><img src="https://res.cloudinary.com/dqggp5pir/image/upload/v1702576198/Language%20Icons/Indeed_logo_ezx44z.png" alt="" /></a>
 
 
 
@@ -95,7 +120,7 @@ function App() {
         </div >
 
 
-      </div>
+      </div >
 
     </>
   )
